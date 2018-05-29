5108b57d728c7d4b22f06e97c7d1493048051c22
Zack Zukowski <drawordoodle@gmail.com>
2018-05-28 21:46:27 -0700

    download from google colab each epoch

--- trainer/plugins.py
+++ trainer/plugins.py
@@ -11,6 +11,7 @@ from torch.utils.trainer.plugins import LossMonitor
 
 from librosa.output import write_wav
 from matplotlib import pyplot
+from google.colab import files
 
 from glob import glob
 import os
@@ -132,6 +133,15 @@ class SaverPlugin(Plugin):
                 )
             )
             self._best_val_loss = cur_val_loss
+        #added by ZVK bellow
+        files.download(
+            os.path.join(
+                self.checkpoints_path,
+                self.best_pattern.format(
+                    epoch_index, self.trainer.iterations
+                )
+            )
+        )
 
     def _clear(self, pattern):
         pattern = os.path.join(self.checkpoints_path, pattern)
